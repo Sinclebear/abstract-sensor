79c53981d0335670bd2d2367a63624ddc77cfbd4
Sinclebear <sanghyukj@gmail.com>
2022-02-07 23:00:19 +0900

    6 passed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-class Sensor {}
+class Sensor {
+    constructor(){
+        this.status = '';
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+    }
+
+    turn(toggle){
+        if(toggle === 'on' && this.powerStatus === 'on') {
+            throw new Error('이미 켜진 상태입니다.');
+        }
+        this.powerStatus = toggle;
+        this.status = 'idle';
+        if(this.status === 'idle'){
+            setTimeout(() => {
+                this.status = 'sensingDistance';
+            },10000);
+        }
+        if(this.status === 'sensingDistance'){
+            setTimeout(() => {
+                this.status = 'reportingData';
+            },500);
+        }
+    }
+}
 
 class IotServer {}
 
